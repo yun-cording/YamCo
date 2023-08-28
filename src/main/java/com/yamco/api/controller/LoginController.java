ed4dd315c00b37e87234ddba0cc8838dae45525f
parkchaelim <pcl8205@naver.com>
2023-08-28 15:54:10 +0900

    Revert "Revert "채림 자체회원가입 중""
    This reverts commit cbbee849e2c55394bb16ec489f520cefedcb6a6d.

---
+++ src/main/java/com/yamco/api/controller/LoginController.java
@@ -0,0 +1,5 @@
+package com.yamco.api.controller;
+
+public class LoginController {
+	
+}
--- src/main/java/com/yamco/user/model/dao/User_DAO.java
+++ src/main/java/com/yamco/user/model/dao/User_DAO.java
@@ -1,5 +1,5 @@
 package com.yamco.user.model.dao;
 
-public class User_Controller {
+public class User_DAO {
 
 }
