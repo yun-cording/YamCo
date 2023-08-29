9c7f890b1fcb6e086ba5f5cb3a0f9aee012eafd8
myNickName <leehanju1208@gmail.com>
2023-08-29 21:45:32 +0900

    230829_lhj_commit

--- src/main/java/com/yamco/user/model/dao/Member_DAO.java
+++ src/main/java/com/yamco/user/model/dao/Member_DAO.java
@@ -1,9 +1,12 @@
 package com.yamco.user.model.dao;
 
+import java.util.List;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.yamco.user.model.vo.Member_Search_VO;
 import com.yamco.user.model.vo.Member_VO;
 
 @Repository
@@ -16,8 +19,13 @@ public class Member_DAO {
 		return sqlSessionTemplate.insert("member.join", mvo);
 	}
 	
-	// 멤버 전체보기
+	// 아이디 중복검사
 	public int getMemberIdChk(String m_id){
 		return sqlSessionTemplate.selectOne("member.idChk", m_id);
+	}
+	
+	//회원 검색
+	public List<Member_VO> getMemberList(Member_Search_VO msvo){
+		return sqlSessionTemplate.selectList("member.selectListBySearchVO", msvo);
 	}
 }
